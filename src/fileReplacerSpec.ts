510d146d931aaeb63d6569818f05c1522158af4c
winton <mail@wintoni.us>
2021-05-11 18:53:03 -0700

    Test conditions

--- src/fileReplacerSpec.ts
+++ src/fileReplacerSpec.ts
@@ -24,11 +24,21 @@ describe("fileReplacer", () => {
           search: /(e)([lt])/g,
           replace: (m, p1, p2) => p1.toUpperCase() + p2,
         },
+        {
+          search: "L",
+          replace: "B",
+          condition: (body) => body.startsWith("LOREM"),
+        },
+        {
+          search: "R",
+          replace: "N",
+          condition: (body) => !body.startsWith("BOREM"),
+        },
       ],
     })
 
     expect(await readFileAsync({ src: tmpPath })).toEqual(
-      "LOREM OPsum dolor sit amEt,\nconsectEtur adopiscing Elit.\n"
+      "BOREM OPsum dolor sit amEt,\nconsectEtur adopiscing Elit.\n"
     )
   })
 })
